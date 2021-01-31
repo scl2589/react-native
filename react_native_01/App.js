50401a1d550bcb26f83b7cef17b065a19b316e88
scl2589 <chaelinshin96@gmail.com>
2021-01-31 17:03:11 +0900

    Add react_native_01 project

---
+++ react_native_01/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Components {
+  render() {
+    return (
+      <View>
+        <Text>hello world</Text>
+      </View>
+    )
+  }
+}
+
+
+export default App;
