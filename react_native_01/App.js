0a8522299f3fded5f343cc271252a4e62060ba7b
scl2589 <chaelinshin96@gmail.com>
2021-01-31 18:28:52 +0900

    Add View, Text

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -6,14 +6,26 @@
  * @flow
  */
 
-import React from 'react';
+//react라는 모듈에서 component라는 클래스를 import 한 것
+import React, { Component } from 'react';
+// View와 Text는 react-native 모듈에서 import 한 것 
 import { View, Text } from 'react-native';
 
-class App extends Components {
+// component를 상속받는 App이라는 클래스를 만들었다.
+class App extends Component {
+  // App 클래스 안에는 화면을 rendering하는 함수가 있다.
   render() {
     return (
       <View>
-        <Text>hello world</Text>
+        <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
       </View>
     )
   }
