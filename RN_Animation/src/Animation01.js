da74924684ea08b48f3fdc98a755ae16007650f3
scl2589 <chaelinshin96@gmail.com>
2021-02-21 20:52:34 +0900

    Add animation interpolation (애니메이셔 효과 조합)

--- RN_Animation/src/Animation01.js
+++ RN_Animation/src/Animation01.js
@@ -32,11 +32,17 @@ class AnimOne extends Component {
             <Animated.View
                 // style={this.state.mySquare.getLayout()}
                 style={{
-                    opacity: this.state.mySquare
+                    opacity: this.state.mySquare,
+                    top: this.state.mySquare.interpolate({
+                        // 숫자가 작은 값부터 넣어줘야 한다.
+                        inputRange: [0, 1],
+                        // 동시에 효과를 내주고 싶은 property 
+                        // property 를 top으로 넣었기 때문에 y축 좌표 값이 들어간다.
+                        outputRange: [700, 0]
+                    })
                 }}
             >
                 <View style={styles.square}>
-
                 </View>
             </Animated.View>
             <Button
