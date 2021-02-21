57e2296ee35af431e25a8b4b498705c496537856
scl2589 <chaelinshin96@gmail.com>
2021-02-21 21:00:11 +0900

    Add animation Interpolation 2

--- RN_Animation/src/Animation01.js
+++ RN_Animation/src/Animation01.js
@@ -33,18 +33,46 @@ class AnimOne extends Component {
                 // style={this.state.mySquare.getLayout()}
                 style={{
                     opacity: this.state.mySquare,
-                    top: this.state.mySquare.interpolate({
-                        // 숫자가 작은 값부터 넣어줘야 한다.
-                        inputRange: [0, 1],
-                        // 동시에 효과를 내주고 싶은 property 
-                        // property 를 top으로 넣었기 때문에 y축 좌표 값이 들어간다.
-                        outputRange: [700, 0]
-                    })
+                    transform: [
+                        {
+                            rotateX: this.state.mySquare.interpolate({
+                                inputRange: [0, 0.5, 1],
+                                outputRange: ['0deg', '180deg', '360deg']
+                            }),
+                        },
+                        {
+                            translateX: this.state.mySquare.interpolate({
+                                inputRange: [0, 0.5, 1],
+                                outputRange: [300, 150, 0]
+                            }),
+                        }
+                    ]
+                    // top: this.state.mySquare.interpolate({
+                    //     // 숫자가 작은 값부터 넣어줘야 한다.
+                    //     inputRange: [0, 1],
+                    //     // 동시에 효과를 내주고 싶은 property 
+                    //     // property 를 top으로 넣었기 때문에 y축 좌표 값이 들어간다.
+                    //     outputRange: [700, 0]
+                    // })
                 }}
             >
                 <View style={styles.square}>
                 </View>
             </Animated.View>
+            <Animated.Text
+                style={{
+                    fontSize: this.state.mySquare.interpolate({
+                        inputRange: [0, 0.5, 1],
+                        outputRange: [40, 30, 20]
+                    }),
+                    color: this.state.mySquare.interpolate({
+                        inputRange:[0, 0.5, 1],
+                        outputRange: ['red', 'green', 'blue']
+                    })
+                }}
+            >
+                <Text>Animation Effects</Text>
+            </Animated.Text>
             <Button
                 title="Animation Start"
                 onPress={this.runAnimation}
