c3b1d6592a64886ba39a0389a3ccb8bf01c7ce62
scl2589 <chaelinshin96@gmail.com>
2021-02-23 01:46:27 +0900

    Platform별 다른 코딩 구현

--- RN_Animation/App.js
+++ RN_Animation/App.js
@@ -7,20 +7,46 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
 // import AnimOne from './src/Animation01';
 // import AnimTwo from './src/Animation02';
 import Supertext from './src/utils/supertext';
 
 class App extends Component {
-  render() {
+  checkSupport = () => {
+    if (Platform.OS === 'ios') {
+      if (Platform.Version < 13.4) {
+        return false
+      }
+    } else {
+      if (Platform.Version < 27) {
+        return false
+      }
+    }
+    return true 
+  }
+
+  render () {
+    // console.warn(Platform.Version)
     return (
       <View style={styles.container}>
-        <Supertext
-          style={{backgroundColor:'red'}}
-        >
-          This is my template!!!!
-        </Supertext>
+        {
+          this.checkSupport() ?
+          <Supertext
+            style={styles.div}
+          >
+            {
+              Platform.OS === 'ios' ? 
+                'This is my iOS Phone'
+                : 
+                'This is my Android Phone'
+            }
+          </Supertext>
+          :
+          <Text>
+            Sorry. Your iphone is not being supported by the app.
+          </Text>
+        }
       </View>
     )
   }
@@ -32,6 +58,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  div: {
+    ...Platform.select({
+      ios: {
+        backgroundColor: 'yellow'
+      },
+      android: {
+        backgroundColor: 'red'
+      }
+    })
   }
 });
 
